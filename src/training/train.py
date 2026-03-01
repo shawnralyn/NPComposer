7338592adea6180531469e4f613af8e93318832f
shawnralyn <shawn.a.ralyn@gmail.com>
2026-03-01 08:08:41 +0000

    minor revisions in training script for v2 run

--- src/training/train.py
+++ src/training/train.py
@@ -6,6 +6,7 @@ import random
 from typing import Any, Dict
 import torch
 from datasets import Dataset
+import sys
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
@@ -13,6 +14,21 @@ from transformers import (
     Trainer,
     TrainingArguments,
 )
+
+# filter 'fast_transformers' warning in stdout
+class LineFilterStream:
+    def __init__(self, stream, banned_substrings):
+        self.stream = stream
+        self.banned = banned_substrings
+    def write(self, s):
+        if any(b in s for b in self.banned):
+            return
+        return self.stream.write(s)
+    def flush(self):
+        return self.stream.flush()
+
+sys.stdout = LineFilterStream(sys.stdout, ["No module named 'fast_transformers'"])
+sys.stderr = LineFilterStream(sys.stderr, ["No module named 'fast_transformers'"])
 
 
 def parse_yaml(yml):
@@ -89,6 +105,7 @@ def update_tokenizer_with_special_tokens(model, tokenizer, special_tokens_dict):
 
     # add special tokens dict to tokenizer vocabulary
     num_added = tokenizer.add_special_tokens(special_tokens_dict)
+    print(f"Added {num_added} special tokens.")
 
     # make model embedding matrix match tokenizer vocabulary size
     if num_added > 0:
@@ -137,6 +154,19 @@ def dataframe_to_tokenized_dataset(
         df[smiles_col].astype(str) +
         (tokenizer.eos_token or "")
     )
+
+    # print statements for dry run
+    # print("\n--- train_text examples ---")
+    # for i, s in enumerate(df["train_text"].head(5).tolist()):
+    #     print(f"[{i}] {s}")
+
+    # print("\n--- tokenization check (special tokens should remain intact) ---")
+    # for s in df["train_text"].sample(n=3, random_state=0).tolist():
+    #     ids = tokenizer(s, add_special_tokens=False)["input_ids"]
+    #     toks = tokenizer.convert_ids_to_tokens(ids)
+    #     print("TEXT:", s[:200], "..." if len(s) > 200 else "")
+    #     print("TOKS:", toks[:60])
+    #     print()
 
     # Filter out rows where tokenized length exceeds filter_len
     df["tokenized_len"] = df["train_text"].apply(
@@ -209,10 +239,6 @@ def conditioning_dropout_collator(tokenizer, special_token_dropout_prob, drop_al
                         
                 smiles_seq["input_ids"] = token_ids # write back the input sequence with stochastic dropout applied
                 smiles_seq["attention_mask"] = [1] * len(token_ids)  # update attention mask with new sequence length
-                
-                # print statements for debugging
-                print("Original:", smiles_seq["input_ids"])
-                print("After dropout:", token_ids)
 
         batch = pad_to_batch(batch_smiles) # pad each sequence in batch to same length (to produce rectangular tensor)                
 
@@ -226,6 +252,8 @@ def conditioning_dropout_collator(tokenizer, special_token_dropout_prob, drop_al
         batch["labels"] = labels
 
         return batch
+
+    return data_collator
 
 
 def main():
@@ -260,8 +288,8 @@ def main():
         args.train_csv, 
         configs["base"]["pathway_col"],
         configs["base"]["superclass_col"],
-        configs["base"]["aromatic_rings_count"],
         configs["base"]["is_glycoside"],
+        configs["base"]["aromatic_rings_count"],
         configs["base"]["qed_bin"],
         configs["base"]["sa_bin"],
     )
@@ -333,7 +361,6 @@ def main():
         warmup_ratio=float(configs["training"]["warmup_ratio"]),
         per_device_train_batch_size=int(configs["training"]["per_device_train_batch_size"]),
         per_device_eval_batch_size=int(configs["training"]["per_device_eval_batch_size"]),
-	    load_best_model_at_end=bool(configs["training"]["load_best_model_at_end"]),
         output_dir=configs["training"]["output_dir"],
         fp16=False,  # turn off mixed precision 
 
@@ -342,7 +369,7 @@ def main():
         run_name=configs["training"]["run_name"],
 
         # Eval/save
-        evaluation_strategy=configs["training"]["evaluation_strategy"],
+        eval_strategy=configs["training"]["evaluation_strategy"],
         save_strategy=configs["training"]["save_strategy"],
 	    save_safetensors=False,
 	    metric_for_best_model=configs["training"]["metric_for_best_model"],
@@ -350,6 +377,15 @@ def main():
         logging_steps=int(configs["training"]["logging_steps"]),
         save_total_limit=int(configs["training"]["save_total_limit"]),
         push_to_hub=bool(configs["training"]["push_to_hub"]),
+        load_best_model_at_end=bool(configs["training"]["load_best_model_at_end"]),
+
+        # dry run
+        # eval_strategy="no",
+        # max_steps=100,
+        # logging_steps=1,
+        # push_to_hub=False,
+        # eval_steps=5,
+        # save_strategy="no",
     )
 
     # Trainer contains all necessary components of a training loop
@@ -365,6 +401,11 @@ def main():
         eval_dataset=ds_val,
         data_collator=data_collator,
     )
+
+    print("CUDA available:", torch.cuda.is_available())
+    if torch.cuda.is_available():
+        print("GPU:", torch.cuda.get_device_name(0))
+    print("Model device (pre-train):", next(model.parameters()).device)
 
     trainer.train()  # starts training
     trainer.save_model()  # save model so you can reload it using "from_pretrained()"
